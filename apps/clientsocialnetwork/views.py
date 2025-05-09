720386f83d3fdde47fb46ea55493a456e391e368
Dani Kensy <kensydanielle@gmail.com>
2025-05-09 13:56:40 -0300

    fix: client class and adding order url patterns

--- apps/clientsocialnetwork/views.py
+++
@@ -1,9 +0,0 @@
-from .models import ClientSocialnetwork
-from rest_framework import viewsets
-from .serializer import ClientSocialnetworkSerializer
-
-# Create your views here.
-
-class ClientSocialnetworkViewSet(viewsets.ModelViewSet):
-    queryset = ClientSocialnetwork.objects.all()
-    serializer_class = ClientSocialnetworkSerializer
